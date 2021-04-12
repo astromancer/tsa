ce9231ba69714e16879977bf22b149dbad83039e
astromancer <hannes@saao.ac.za>
2021-04-12 15:57:09 +0200

    move tests

--- tsa/spectral.py
+++ tsa/spectral.py
@@ -572,35 +572,3 @@ def show_all_windows(cmap='gist_rainbow'):
             pass
     plt.legend()
     plt.show()
-
-
-if __name__ == '__main__':
-
-    def check_parceval(signal, periodogram):
-        # Parceval's theorem
-        tp_signal = np.square(signal).sum()
-        tp_fft = periodogram.sum() / len(signal)
-        return np.allclose(tp_signal, tp_fft)
-
-
-    def check_DC(signal, periodogram):
-        # DC component is squared signal sum
-        ss = signal.sum()
-        return (ss * ss) == periodogram[0]
-
-
-    def check_var_rms_relation(signal, periodogram):
-        """Variance of a real ts is equal the rms of the non-DC power spectrum"""
-        n = len(signal)
-        rms_pwr = periodogram[1:].sum() / (n * n)
-        var = np.var(signal)
-        return np.allclose(var, rms_pwr)
-
-
-    # check parceval for even and odd signals
-    off = 1e4
-    N = 2 ** 10
-    for n in [N, N - 1]:
-        signal = np.random.randn(n) + off
-        pwr = periodogram(signal)
-        check_parceval(signal, pwr)
