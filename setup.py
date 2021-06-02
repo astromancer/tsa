3728fd5bbf5eeb10590e3174c987b23fa9e1783c
astromancer <hannes@saao.ac.za>
2021-06-02 09:29:37 +0200

    add install files

--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
 
 import site
-import inspect
-import subprocess
-from pathlib import Path
+import sys
+import setuptools
 
-
-def link_into_path():
-    """
-    Simple install strategy that links the repo's man directory into system python path.
-    """
-    here = inspect.getfile(inspect.currentframe())
-    here = Path(here)
-    src = here.parent.resolve()
-    pkg_name = src.name
-    src = str(src)
-    dest = site.getsitepackages()[0]
-    dest = str(Path(dest) / pkg_name)
-
-    #print(src, dest)
-    subprocess.call(['ln','-s', src, dest])
-    # ln -s path/to/repo/eeg `python3 -m site --user-site`/eeg
-
-
-link_into_path()
+# see: https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = ('--user' in sys.argv[1:])
+setuptools.setup()
