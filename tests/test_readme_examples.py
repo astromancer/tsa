23c7993d347ebfb74671751cd7f0220f8f565ac6
astromancer <hannes@saao.ac.za>
2021-06-18 12:57:34 +0200

    update readme, examples

--- tests/test_readme_examples.py
+++ tests/test_readme_examples.py
@@ -1,20 +1,23 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
 # pylint: disable=redefined-outer-name
+# pylint: disable=invalid-name
+# pylint: disable=unused-import
 
 
 # std libs
 import re
 import itertools as itt
 from pathlib import Path
-import matplotlib.pylab as plt
 
 # third-party libs
 import pytest
-
 import numpy as np
+import matplotlib.pyplot as plt
 
-from tsa import TimeSeries
+
+from tsa.ts import TimeSeries
 from tsa.spectral import Periodogram
 from tsa.ts.generate import Harmonic
 
@@ -39,16 +42,18 @@ class get_name:
 #         if isinstance(obj, (TimeSeries, Periodogram)):
 #             return obj.fig
 
+
 @pytest.fixture(params=itt.islice(get_readme_examples(), 3),
-                                 ids=get_name())
+                ids=get_name())
 def readme_code(request):
     return request.param
 
 
-@pytest.mark.mpl_image_compare(baseline_dir='images')
+@pytest.mark.mpl_image_compare(baseline_dir='images', style='default')
 def test_readme_example(readme_code):
     locals_ = {}
-    code = f'{readme_code}\nfig = plt.gcf()'
+    code = (readme_code + '\n'
+            'fig = plt.gcf()')
     exec(code, None, locals_)
     globals().update(locals_)
     return locals_['fig']
